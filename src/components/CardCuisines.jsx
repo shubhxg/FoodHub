555fa8cc85a523cd0291897aec1a57dd1439ad3d
Shubh Sharma <69891912+shubhxg@users.noreply.github.com>
2024-06-28 16:50:56 +0530

    Create CardCuisines.jsx

---
+++ src/components/CardCuisines.jsx
@@ -0,0 +1,15 @@
+import { CUISINES_IMG_URL } from "../utils/data/constants";
+
+export default function CardCuisines(props) {
+  const { imageId } = props;
+
+  return (
+    <div className="w-32 -mb-8 cursor-pointer">
+      <img
+        src={CUISINES_IMG_URL + imageId}
+        alt="cuisine picture"
+        className="rounded-md"
+      />
+    </div>
+  );
+}
