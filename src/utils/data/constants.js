fecba7a5d7c0c06a62867b9223347e592d7deb83
Shubh Sharma <69891912+shubhxg@users.noreply.github.com>
2024-06-28 16:56:07 +0530

    Create constants.js

---
+++ src/utils/data/constants.js
@@ -0,0 +1,17 @@
+export const LOGO_URL =
+  "https://www.thefoodaholic.co.uk/wp-content/uploads/2014/05/MgJ-nIJ2.png";
+
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const restaurantDataAPIURL =
+  "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+
+export const MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.3670355&lng=79.4304381&restaurantId=";
+
+export const MENU_IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_150,h_150,c_fit/";
+
+export const CUISINES_IMG_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
