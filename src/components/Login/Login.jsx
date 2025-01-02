d23a3afdbeff5c13b8120e10c5bf1908eb477362
Shubh Sharma <mail.shubhsharma19@gmail.com>
2025-01-02 20:19:36 +0530

    style: enhance Login component UI

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -20,21 +20,21 @@ export default function Login() {
   };
 
   return (
-    <div className="md:min-h-screen md:flex md:items-center md:justify-center bg-gray-100 min-w-screen">
-      <div className="bg-white p-8 rounded-xl shadow-md w-auto md:flex md:items-center justify-start gap-8">
+    <div className="md:h-full py-16 md:flex md:items-center md:justify-center bg-gray-100 min-w-screen">
+      <div className="bg-white pr-8 rounded-xl md:shadow-[0_35px_60px_-30px_rgba(0,0,0,0.2)] w-auto md:flex md:items-center justify-start gap-8">
         <div>
           <figure>
             <img
-              className="md:w-auto md:max-h-2/6 object-cover rounded-xl md:block hidden"
+              className="md:w-96 object-cover md:rounded-l-lg md:block hidden"
               src="https://images.pexels.com/photos/842571/pexels-photo-842571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="food image"
             />
           </figure>
         </div>
-        <div className="form-container w-3/5">
-            <h2 className="text-2xl font-bold text-rose-600 mb-6 underline underline-offset-2">
-              Login to FoodPalace
-            </h2>
+        <div className="form-container w-3/5 p-8">
+          <h2 className="text-2xl font-bold text-rose-600 mb-6 underline underline-offset-2">
+            Login to FoodPalace
+          </h2>
           <form onSubmit={handleSubmit} className="space-y-4">
             <div>
               <label
